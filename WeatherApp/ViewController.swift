9c4be18ec1106207b87d1016f68ad2184703050e
Susan Zheng <Zheng.susan12@gmail.com>
2019-06-07 13:12:56 -0400

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Susan Zheng on 6/7/19.
+//  Copyright © 2019 Susan Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
